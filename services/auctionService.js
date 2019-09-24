015f788293f6dee36683dfd9227a207494db82b0
Hjörtur Jóhann Vignisson <hjorturv17@ru.is>
2019-09-24 23:04:53 +0000

    place bid done

--- services/auctionService.js
+++ services/auctionService.js
@@ -20,12 +20,11 @@ const auctionService = () => {
                 body: result
             }
         });
-        
     };
 
-    const getAuctionWinner = async (_auctionId) => {
+    const getAuctionWinner = async (auctionId) => {
         return await globalTryCatch(async () => {
-            const result = await dbProvider.Auction.findById(_auctionId);
+            const result = await dbProvider.Auction.findById(auctionId);
             if(result == null) {
                 return {
                     status: 404,
@@ -40,14 +39,14 @@ const auctionService = () => {
                 };
             }
 
-            const highestBid = await dbProvider.AuctionBid.find({ auctionId: _auctionId }).sort({ price: -1 });
+            const highestBid = await dbProvider.AuctionBid.find({ auctionId: auctionId }).sort({ price: -1 });
             if(highestBid.length == 0) {
                 return { 
                     status: 200, 
                     body: "This auction has no bids" 
                 };
             }
-            
+
             const customer = await dbProvider.Customer.findById(highestBid[0].customerId);
             return { 
                 status: 200, 
@@ -65,6 +64,7 @@ const auctionService = () => {
                     body: "Conflict! - Auction already exists for this art item"
                 }
             }
+
             const art = await dbProvider.Art.findById(auction.artId);
             if(art == null) {
                 return {
@@ -72,12 +72,14 @@ const auctionService = () => {
                     body: "Art work not found"
                 }
             }
+
             if(!art.isAuctionItem) {
                 return {
                     status: 412,
                     body: "This art work is not an auction item"  
                 }
             }
+
             const result = await dbProvider.Auction.create(auction);
             return {
                 status: 201,
@@ -87,12 +89,78 @@ const auctionService = () => {
         });
     };
 
-	const getAuctionBidsWithinAuction = (auctionId, cb, errorCb) => {
-        // Your implementation goes here
+	const getAuctionBidsWithinAuction = async (auctionId) => {
+        return await globalTryCatch(async () => {
+            const auctionValidation = await dbProvider.Auction.findById(auctionId);
+            if(auctionValidation == null) {
+                return {
+                    status: 404,
+                    body: "Auction with this id was not found"
+                }
+            }
+
+            const bids = await dbProvider.AuctionBid.find({ auctionId: auctionId }).sort({ price: -1 });
+            return {
+                status: 200,
+                body: bids
+            }
+        });
     };
 
-	const placeNewBid = (auctionId, customerId, price, cb, errorCb) => {
-		// Your implementation goes here
+	const placeNewBid = async (auctionId, customerId, price) => {
+        //Validate auction id
+        const auction = await dbProvider.Auction.findById(auctionId);
+        if(auction == null) {
+            return {
+                status: 404,
+                body: "Auction with this id was not found!"
+            }
+        }
+
+        //Validate customer id
+        const customer = await dbProvider.Customer.findById(customerId);
+        if(customer == null) {
+            return {
+                status: 404,
+                body: "Customer with this id was not found!"
+            }
+        }
+
+        //Check if auction is over (date)
+        if(auction.endDate < Date.now()) {
+            return {
+                status: 403,
+                body: "Forbidden! - Auction is already over!"
+            }
+        }
+
+        //Find all bids for this auction, order by price
+        //Check if price is higher than highest bid
+        const highestBid = await dbProvider.AuctionBid.find({ auctionId: auctionId }).sort({ price: -1 });
+
+        //Check if price is higher than minimum price
+        if(auction.minimumPrice >= price || highestBid[0].price >= price) {
+            return {
+                status: 412,
+                body: "Bidding price too low!"
+            }
+        }
+
+        //Create the bid
+        await dbProvider.AuctionBid.create({
+            auctionId: auctionId,
+            customerId: customerId,
+            price: price
+        });
+
+        //Update auction winner prtop of auction to customerId if everything valid
+        await dbProvider.Auction.findById(auctionId).updateOne({ auctionWinner: customerId });
+        //console.log(stuff);
+
+        return {
+            status: 202,
+            body: "Success - Your bid has been placed!"
+        }
 	}
 
     return {
